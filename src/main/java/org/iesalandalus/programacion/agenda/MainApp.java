346d88bd77e134cd2610194adda9985c7e89c4d2
Galina K. <galinakatulskaja1228@list.ru>
2018-12-04 13:23:04 +0100

    1)creados los método menu ,los métodos privados mostrar menu, elegirOpcion, ejecutarOpcion

--- src/main/java/org/iesalandalus/programacion/agenda/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/agenda/MainApp.java
@@ -7,5 +7,62 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		System.out.println("Programa para gestionar una agenda de contactos");
+                
+            int opcion;
+                do 
+                {
+                    mostrarMenu();
+                    opcion = elegirOpcion();
+                    ejecutarOpcion(opcion);
+                } while (opcion != 0);
+
+                System.out.println("Fin del trabajo con Agenda!!!!");
+            }
+        private static void mostrarMenu() 
+    {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("--------------------------------------------------");
+	System.out.println("Elige la opción que desear realizar");
+	System.out.println("--------------------------------------------------");
+	System.out.println("");
+        System.out.println("1.- Añadir contacto");
+        System.out.println("2.- Buscar contacto");
+        System.out.println("3.- Borrar contacto");
+	System.out.println("4.- Listar Agenda");	
+	System.out.println("");
+	System.out.println("0.- Salir.");
+	System.out.println("");
+    }
+        private static int elegirOpcion() 
+    {
+	int opcion;
+	do 
+        {
+            System.out.println("Elige una opción (0-6): ");
+            opcion = Entrada.entero();
+	} while (opcion < 0 || opcion > 4);
+
+        return opcion;
+    }
+        private static void ejecutarOpcion(int opcion)
+        {
+            switch(opcion)
+            {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:   
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                    
+            }        
+            
         }
 }
